00219d0a1fbb848dbcc496b0670027d307c9f037
sboyarkin <sergey.boyarkin@mail.ru>
2025-06-05 10:41:59 +0500

    class columnWidget.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./css/style.css";
 
 import "./js/app";
-import {ColumnWidget} from "./js/columnWidget";
+import { ColumnWidget } from "./js/columnWidget";
 // TODO: write your code in app.js
 
+const todo = document.querySelector(".todo");
 
-const todo = document.querySelector('todo')
+const colWindget = new ColumnWidget(todo);
 
-
+todo.appendChild(colWindget.createCol())
 
--- src/js/columnWidget.js
+++ src/js/columnWidget.js
@@ -1,10 +1,31 @@
 export class ColumnWidget {
-  constructor(element) {
-    this.element = element;
-    this.tit = ["TODO", "IN PROGRESS", "DONE"];
+  constructor(el) {
+    this.title = ["TODO", "IN PROGRESS", "DONE"];
+    this.el = el;
   }
 
-  createCol() {
-
+  createCol(el) {
+    this.title.forEach((e) => {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      const title = document.createElement("div");
+      title.classList.add("title");
+      title.textContent = e;
+      const addItem = document.createElement("div");
+      addItem.classList.add("addItem");
+      (addItem.innerHTML =
+        '       <div class="add-btn"> + Add another card</div>\n' +
+        '       <div class="input">\n' +
+        '         <input class="text-input" type="text">\n' +
+        '         <div class="d-flex">\n' +
+        '           <div class="g-btn">Add Card</div>\n' +
+        '           <div class="closes-btn"></div>\n' +
+        "         </div>\n" +
+        "       </div>\n" +
+        "     </div>"),
+        column.append(title);
+      column.append(addItem);
+      this.el.append(column)
+    });
   }
 }
